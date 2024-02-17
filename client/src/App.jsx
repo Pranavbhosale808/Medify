8026f9093484ea285526cfc8105f244ab549d85f
geekclick <anujghom27@gmail.com>
2024-02-17 10:06:53 +0530

    old frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {Home} from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Login } from "./pages/Login";
@@ -8,24 +8,23 @@ import { Navbar } from "./Components/Navbar";
 import Profile from "./pages/Profile";
 import { Logout } from "./pages/Logout";
 
-
-const App = ()=>{
-return <>
-
-<BrowserRouter>
-<Navbar/>
-<Routes>
-  <Route path="/" element={<Home />}/>
-  <Route path="/About" element={<About/>}/>
-  <Route path="/Contact" element={<Contact/>}/>
-  <Route path="/login" element={<Login/>}/>
-  <Route path="/signup" element={<Register/>}/>
-  <Route path="/Profile" element={<Profile/>}/>
-  <Route path="/logout" element={<Logoutt/>}/>
-</Routes>
-</BrowserRouter>
-
-</>
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
